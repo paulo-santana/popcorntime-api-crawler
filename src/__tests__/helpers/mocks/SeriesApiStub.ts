98e9424506b500bfda8815979bf789c054a7da7a
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-12 16:59:03 -0300

    refactor(stub): make SeriesApiStub work with new ApiResources type

--- src/__tests__/helpers/mocks/SeriesApiStub.ts
+++ src/__tests__/helpers/mocks/SeriesApiStub.ts
@@ -1,29 +1,27 @@
-import { IPopcornTimeApi, PopcornApiStatus } from '@/services'
+import { IPopcornTimeApi } from '@/services'
 import { PopcornShow } from '@/services/popcornTimeTypes'
-import { apiStatusStub } from './mocks'
-import apiResources from './apiResources'
+import { apiResources } from './apiResources'
 
-export const pages = ['shows/1', 'shows/2']
+const { shows } = apiResources
 
 export class SeriesApiStub implements IPopcornTimeApi {
-  getStatus(): Promise<PopcornApiStatus> {
-    const statusStub: PopcornApiStatus = apiStatusStub
-    return new Promise(resolve => resolve(statusStub))
-  }
+  pages = ['shows/1', 'shows/2']
+  popcornShows: PopcornShow[] = new Array<PopcornShow>().concat(
+    ...shows['shows/1'],
+    ...shows['shows/2']
+  )
 
   getPages(): Promise<string[]> {
-    return new Promise(resolve => resolve(pages))
+    return new Promise(resolve => resolve(this.pages))
   }
 
   getByPage(page: 'shows/1' | 'shows/2'): Promise<PopcornShow[]> {
-    const response = apiResources[page]
+    const response = shows[page]
     return new Promise(resolve => resolve(response))
   }
 
   getById(id: string): Promise<PopcornShow> {
-    const shows = apiResources['shows/1']
-    shows.concat(...apiResources['shows/2'])
-    const foundShow = shows.find(show => show._id === id)
+    const foundShow = this.popcornShows.find(show => show._id === id)
     return new Promise(resolve => resolve(foundShow))
   }
 }
