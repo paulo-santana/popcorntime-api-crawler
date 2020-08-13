89c2a079177ac0b5f32e0347b9f55da4a3de07e3
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-13 13:42:20 -0300

    test: make sure AnimesApi fetches each page and return its values correctly

--- src/__tests__/helpers/httpClientFactory.ts
+++ src/__tests__/helpers/httpClientFactory.ts
@@ -1,9 +1,17 @@
 import { IHttpClient } from '@/services/HttpClient'
+import { apiResources, animesPages } from './mocks/mocks'
+
+const responsesForUris = {
+  animes: animesPages.map(v => v),
+  'animes/1': apiResources.animes['animes/1'],
+  'animes/2': apiResources.animes['animes/2'],
+}
 
 const makeHttpClient = (): IHttpClient => {
   const httpClient: IHttpClient = {
-    get(uri: string): Promise<string> {
-      return new Promise(resolve => resolve(uri))
+    get(uri: 'animes' | 'animes/1' | 'animes/2'): Promise<string> {
+      const response = responsesForUris[uri]
+      return new Promise(resolve => resolve(JSON.stringify(response)))
     },
   }
   return httpClient
--- src/__tests__/unit/AnimesApi.test.ts
+++ src/__tests__/unit/AnimesApi.test.ts
@@ -1,10 +1,16 @@
 import { AnimesApi } from '@/data/api'
+import { apiResources } from '../helpers/mocks/mocks'
 import makeHttpClient from '../helpers/httpClientFactory'
+
+const makeSut = () => {
+  const httpClient = makeHttpClient()
+  const animesApi = new AnimesApi(httpClient)
+  return { animesApi, httpClient }
+}
 
 describe('AnimesApiAccess', () => {
   it('should fetch and parse pages correctly', async () => {
-    const httpClient = makeHttpClient()
-    const animesApi = new AnimesApi(httpClient)
+    const { animesApi, httpClient } = makeSut()
     jest.spyOn(httpClient, 'get').mockImplementationOnce(() => {
       return new Promise(resolve =>
         resolve(JSON.stringify(['animes/1', 'animes/2']))
@@ -12,5 +18,14 @@ describe('AnimesApiAccess', () => {
     })
     const pages = await animesApi.getPages()
     expect(pages).toEqual(['animes/1', 'animes/2'])
+  })
+
+  it('should be able to get animes for each page', async () => {
+    const { animesApi } = makeSut()
+    const firstPage = await animesApi.getByPage('animes/1')
+    expect(firstPage).toEqual(apiResources.animes['animes/1'])
+
+    const secondPage = await animesApi.getByPage('animes/2')
+    expect(secondPage).toEqual(apiResources.animes['animes/2'])
   })
 })
