9625e8c4460d08dee811dab01cd1a0a2c095cbe2
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-14 00:33:15 -0300

    test: make sure AxiosApiClient fetches data and returns it as string

---
+++ src/__mocks__/axios.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const mockAxios: typeof axios = jest.genMockFromModule('axios')
+
+mockAxios.create = jest.fn(() => mockAxios)
+
+export default mockAxios
---
+++ src/__tests__/unit/httpClients/AxiosHttpClient.test.ts
@@ -0,0 +1,22 @@
+import { mocked } from 'ts-jest/utils'
+import { AxiosHttpClient } from '@/services/HttpClient'
+import { apiStatusStub } from '@/__tests__/helpers/mocks/mocks'
+import axios from 'axios'
+
+const mockedAxios = mocked(axios, true)
+
+const makeSut = () => {
+  const axiosClient = new AxiosHttpClient('https://movies-v2.api-fetch.sh')
+  return { axiosClient }
+}
+
+describe('AxiosHttpClient', () => {
+  it('should fetch endpoints and return response as string', async () => {
+    const { axiosClient } = makeSut()
+    mockedAxios.get.mockResolvedValueOnce({
+      data: JSON.stringify(apiStatusStub),
+    })
+    const response = await axiosClient.get('status')
+    expect(response).toEqual(JSON.stringify(apiStatusStub))
+  })
+})
