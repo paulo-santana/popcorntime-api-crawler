c85604d6db28b2a808f7d22e9078c3e948dbca15
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-15 22:52:48 -0300

    feat: create SeriesRepository class

--- src/data/database/IDatabase.ts
+++
@@ -1,4 +0,0 @@
-export interface IDatabaseClient<T = Movie | Series | Anime> {
-  saveMany(items: T[]): Promise<void>
-  getAll(): Promise<Array<Movie>>
-}
--- src/data/repositories/SeriesRepository.ts
+++ src/data/repositories/SeriesRepository.ts
@@ -1,6 +1,23 @@
-import { Series } from '../models/Series'
+import { Series } from '@/data/models/Series'
+import { IDatabaseClient } from '@/data/database/IDatabaseClient'
 
 export interface ISeriesRepository {
   getAll(): Promise<Series[]>
   saveMany(series: Series[]): void
+}
+
+export class SeriesRepository {
+  databaseClient: IDatabaseClient
+
+  constructor(databaseClient: IDatabaseClient) {
+    this.databaseClient = databaseClient
+  }
+
+  async saveMany(items: Series[]): Promise<void> {
+    await this.databaseClient.saveMany(items)
+  }
+
+  getAll(): Promise<Series[]> {
+    return this.databaseClient.getAll()
+  }
 }
