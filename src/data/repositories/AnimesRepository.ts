aa20a33b8149e6a8c189519e8229f1f59fbaec72
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-15 23:11:29 -0300

    feat: make sure AnimesRepository uses client correctly

--- src/data/repositories/AnimesRepository.ts
+++ src/data/repositories/AnimesRepository.ts
@@ -1,6 +1,23 @@
 import { Anime } from '../models/Anime'
+import { IDatabaseClient } from '../database/IDatabaseClient'
 
 export interface IAnimesRepository {
   getAll(): Promise<Anime[]>
   saveMany(animes: Anime[]): void
+}
+
+export class AnimesRepository implements IAnimesRepository {
+  databaseClient: IDatabaseClient
+
+  constructor(databaseClient: IDatabaseClient) {
+    this.databaseClient = databaseClient
+  }
+
+  async getAll(): Promise<Anime[]> {
+    return this.databaseClient.getAll()
+  }
+
+  async saveMany(animes: Anime[]): Promise<void> {
+    await this.databaseClient.saveMany(animes)
+  }
 }
