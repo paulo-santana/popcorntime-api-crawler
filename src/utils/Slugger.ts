cc1330f8ea48cc5c9b8e44788ef4873d5aca8df4
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-17 00:04:29 -0300

    fix: make slugger slug correctly

--- src/utils/Slugger.ts
+++ src/utils/Slugger.ts
@@ -7,7 +7,36 @@ export class Slugger implements ISlugger {
   slugWithId(title: string, id: string): string {
     return `${title}-${id}`
   }
+
   slug(title: string): string {
-    return title
+    return this.slugify(title)
+  }
+
+  // Slugify a string
+  private slugify(text: string) {
+    let str = text.replace(/^\s+|\s+$/g, '')
+
+    // Make the string lowercase
+    str = str.toLowerCase()
+
+    // Remove accents, swap ñ for n, etc
+    const from =
+      'ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆaİĞŞığş·/_,:;'
+    const to =
+      'AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAaIGSigs------'
+
+    for (let i = 0, l = from.length; i < l; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+    }
+
+    // Remove invalid chars
+    str = str
+      .replace(/[^a-z0-9 -]/g, '')
+      // Collapse whitespace and replace by -
+      .replace(/\s+/g, '-')
+      // Collapse dashes
+      .replace(/-+/g, '-')
+
+    return str
   }
 }
