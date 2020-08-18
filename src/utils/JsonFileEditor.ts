e74b14fcf730da0cd382a20137ba89d7b8f93308
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-18 14:47:00 -0300

    fix: check if file exists before trying to open it

--- src/utils/JsonFileEditor.ts
+++ src/utils/JsonFileEditor.ts
@@ -8,12 +8,16 @@ export class JsonFileEditor implements IStorageManager {
   async getData(key: string): Promise<unknown> {
     const file = path.resolve(this.dir, '..', '..', 'files', key)
     return new Promise((resolve, reject) => {
-      fs.readFile(file, { encoding: 'utf-8' }, (error, data) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(data)
-      })
+      if (fs.existsSync(file)) {
+        fs.readFile(file, { encoding: 'utf-8' }, (error, data) => {
+          if (error) {
+            reject(error)
+          }
+          resolve(data)
+        })
+      } else {
+        resolve()
+      }
     })
   }
 
