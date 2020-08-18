b46514560b92c90b6b6d7dd18795ee73dd349f6b
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-18 13:55:33 -0300

    refactor(scheduler): add dependency injection

--- src/app/scheduler/Scheduler.ts
+++ src/app/scheduler/Scheduler.ts
@@ -1,19 +1,25 @@
 import nodeCron, { ScheduledTask } from 'node-cron'
 import { Logger, ILogger } from '../logger/Logger'
 
+export type SchedulerConfigTypes = {
+  logger?: ILogger
+  loggingActive?: boolean
+}
+
 export class NodeCronScheduler {
   scheduledTask: ScheduledTask
   jobs: Array<() => void>
 
   private logger: ILogger
-  private loggingActive = true
+  private loggingActive = false
 
-  constructor(schedule = '0 * * * *') {
+  constructor(schedule = '0 * * * *', config?: SchedulerConfigTypes) {
     this.jobs = new Array<() => void>()
     this.scheduledTask = nodeCron.schedule(schedule, () => this.runJobs(), {
       scheduled: false,
     })
-    this.logger = new Logger()
+    this.logger = config?.logger || new Logger()
+    this.loggingActive = config?.loggingActive || false
   }
 
   private log(message: string) {
@@ -24,7 +30,7 @@ export class NodeCronScheduler {
 
   start(): void {
     this.scheduledTask.start()
-    this.log(`status: ${this.scheduledTask.getStatus()}`)
+    this.log(`schedule started!`)
   }
 
   stop(): void {
